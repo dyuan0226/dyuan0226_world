f34a6fe51fe25f8cd57e5a13e289c04933bb5506
caitlinhuang <caitlinhuang8@gmail.com>
2021-03-09 16:03:44 -0800

    added controllers

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
+  should belong_to(:category)
+  should validate_presence_of(:title
+  should validate_presence_of(:content)
+
+  
   #   assert true
   # end
 end
